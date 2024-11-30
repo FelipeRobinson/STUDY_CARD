6d25e6372f8527a99a33c6fdd82af460cea31fb6
FelipeRobinson <feliperobinsonprofissional@gmail.com>
2024-11-30 16:51:45 -0300

    Update

---
+++ LINGUAGENS/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'PORTUGUÊS',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+);
+
+criaCartao(
+    'INGLÊS',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+);
+
+criaCartao(
+    'ED. FÍSICA',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+);
+
+criaCartao(
+    'ARTES',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
