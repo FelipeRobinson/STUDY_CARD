e557433c2e459a115e8204840170ddce056f41a3
FelipeRobinson <feliperobinsonprofissional@gmail.com>
2024-12-01 10:22:31 -0300

    update

--- LINGUAGENS/perguntas.js
+++ LINGUAGENS/perguntas.js
@@ -1,13 +1,13 @@
 criaCartao(
     'PORTUGUÊS',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'O que é um texto dissertativo-argumentativo, e qual é a sua estrutura básica?',
+    'Um texto dissertativo-argumentativo é um tipo textual que apresenta um ponto de vista sobre um tema e o defende com argumentos. Sua estrutura básica inclui: Introdução; Desenvolvimento; Conclusão.'
 );
 
 criaCartao(
-    'INGLÊS',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'PORTUGUÊS',
+    'Quais são as principais características do Modernismo no Brasil?',
+    'O Modernismo no Brasil é caracterizado pela valorização da identidade nacional, a experimentação de novas formas de linguagem e a ruptura com os padrões tradicionais.'
 );
 
 criaCartao(
@@ -15,6 +15,24 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 );
+
+criaCartao(
+    'ARTES',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'ARTES',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'ARTES',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
 
 criaCartao(
     'ARTES',
